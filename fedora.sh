2de95ea615810bf3291c3e1b7db2e72df5e3abcb
asdf <asdf@snakebite.com>
2014-10-16 15:01:35 +0100

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -3,9 +3,6 @@
 # Skeleton script to load a particular postinstall
 # script version depending on your Fedora version.
 
-# For now, it is simply hardcoded to download the
-# F20 script.
-
 /bin/echo "This script will now attempt to check for and download
 a compatible Fedora PostInstall script for your version of Fedora.
 
