22b76a7a4fa88daa038c90e4cfdf6de262f142d8
asdf <asdf@snakebite.com>
2014-12-06 17:48:58 +0000

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -3,14 +3,10 @@
 # Skeleton script to load a particular postinstall
 # script version depending on your Fedora version.
 
-/bin/echo "
-
-This script will now attempt to check for and download
-a compatible Fedora PostInstall script for your version of Fedora.
-
-If the download fails, then unfortunately there is currently
-no script available for your Fedora version.
-
-"
+echo "This script will now attempt to check for and download"
+echo "a compatible Fedora PostInstall script for your version of Fedora."
+echo ""
+echo "If the download fails, then unfortunately there is probably"
+echo "no script available for your Fedora version yet."
 
 curl -o /tmp/fedora$(rpm -E %fedora).sh https://raw.githubusercontent.com/asdf-git/Fedora-PostInstall/master/fedora$(rpm -E %fedora).sh && sudo sh /tmp/fedora$(rpm -E %fedora).sh
