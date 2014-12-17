8cf460bdb2f5efa7924d59c5a2911014d826fa0e
gelrii <jsmorgan1993@gmail.com>
2014-12-17 23:17:46 +0000

    Create Fedora21.sh

---
+++ Fedora21.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+gituser=asdf-git
+
+continue=1
+
+# curl -fsSL https://raw.githubusercontent.com/$gituser/Fedora-PostInstall/master/fedora$(rpm -E %fedora).sh -o /tmp/fedora.sh && sudo sh /tmp/fedora.sh
+
+# Fedora 21 PostInstall Script
+version="2.0 (20141206)"
+fvsupp="21" #(Fedora release version this script targets)
+# Check if we're running as root
+if (( $EUID != 0 )); then
+/bin/echo "This script must be run as root. Type in 'sudo $0' to run it as root."
+exit 1
+fi
+# Fedora Version Check
+if (( $(rpm -E %fedora) != $fvsupp )); then
+/bin/echo "This script is optimised for Fedora $fvsupp.
+It may not run correctly on earlier/later versions of Fedora.
+To force the script to run on this system, please remove the
+\"Fedora Version Check\" section of the script, and attempt
+to run the script again."
+exit 1
+fi
+# Fedora Version Check
+
+#dir="$(dirname "$0")"
+dir="$(dirname "$0")/Functions/"
+
+. $dir/Start
+. $dir/Main
+#. $dir/Exit
+. $dir/Enable_RPMFusion
+. $dir/Desktop_Environment
+. $dir/Remove_KDE_Packages
+. $dir/Remove_Gnome_Packages
+. $dir/System_Update
+. $dir/Installer
+. $dir/Install_Packages
+. $dir/Enable_SSH
+. $dir/Install_ZoL
+. $dir/Hosts_Block
+. $dir/Fix_Fonts
+. $dir/Install_ownCloud
+. $dir/Install_POL
+#. $dir/
+
+until [ $continue = 0 ]; do
+	Start
+exit
+done
