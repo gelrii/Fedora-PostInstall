9f98d9a75ce6e28f42b6e08aeab63f8636512405
asdf <asdf@snakebite.com>
2014-12-18 00:04:01 +0000

    Update Fedora.sh

--- Fedora.sh
+++ Fedora.sh
@@ -4,7 +4,7 @@
 #This is useful if you have forked the script and intend to customise your own plugins.
 gituser=asdf-git
 
-yum install git -y
-mkdir /tmp/postinst
-cd /tmp/postinst
-git clone -q https://github.com/asdf-git/Fedora-PostInstall.git
+sudo yum install git -y
+cd /tmp
+git clone -q https://github.com/$gituser/Fedora-PostInstall.git
+sudo sh /tmp/Fedora-PostInstall/Fedora$(rpm -E %fedora).sh
