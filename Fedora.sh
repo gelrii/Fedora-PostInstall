2f33d6d828bf63902baaaa628210dbb30628d3b6
asdf <asdf@snakebite.com>
2015-01-05 04:21:16 +0000

    Removed install of git dependancy - already present on F21

--- Fedora.sh
+++ Fedora.sh
@@ -4,7 +4,6 @@
 #This is useful if you have forked the script and intend to customise your own plugins.
 gituser=asdf-git
 
-sudo yum install git -y
 rm -rf /tmp/Fedora-PostInstall
 cd /tmp
 git clone -q https://github.com/$gituser/Fedora-PostInstall.git
