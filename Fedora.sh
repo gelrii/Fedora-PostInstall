078cb78e20b7d0b1624b91e4c50d399e5c7a953e
asdf <asdf@snakebite.com>
2015-02-22 04:03:28 +0000

    Update Fedora.sh

--- Fedora.sh
+++ Fedora.sh
@@ -3,9 +3,9 @@
 #Configure the GitHub user that the plugin scripts will be pulled from.
 #This is useful if you have forked the script and intend to customise your own plugins.
 gituser=asdf-git
-luser=`whoami` #Set the identity of the user who launched the script. Will be useful later.
 
 rm -rf /tmp/Fedora-PostInstall
 cd /tmp
+echo `whoami` > /tmp/Fedora-PostInstall/user
 git clone -q https://github.com/$gituser/Fedora-PostInstall.git
 sudo sh /tmp/Fedora-PostInstall/Fedora$(rpm -E %fedora).sh
