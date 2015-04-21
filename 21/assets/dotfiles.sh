a18fad81df79ce444c5da6d18cee08948064b7df
gelrii <jsmorgan1993@gmail.com>
2015-04-21 22:52:35 +0100

    Update dotfiles.sh

--- 21/assets/dotfiles.sh
+++ 21/assets/dotfiles.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-curl -o /home/`whoami`/.bashrc https://raw.githubusercontent.com/asdf-git/dotfiles/master/.bashrc
-curl -o /home/`whoami`/.bash_aliases https://raw.githubusercontent.com/asdf-git/dotfiles/master/.bash_aliases
-curl -o /home/`whoami`/.systeminfo.sh https://raw.githubusercontent.com/asdf-git/dotfiles/master/.systeminfo.sh
-curl -o /home/`whoami`/.update.sh https://raw.githubusercontent.com/asdf-git/dotfiles/master/.update.sh
+curl -o /home/`whoami`/.bashrc https://raw.githubusercontent.com/gelrii/dotfiles/master/.bashrc
+curl -o /home/`whoami`/.bash_aliases https://raw.githubusercontent.com/gelrii/dotfiles/master/.bash_aliases
