14797297d63ce7963b734722babf08162d821943
gelrii <jsmorgan1993@gmail.com>
2015-04-22 00:31:43 +0100

    Delete dotfiles.sh

--- 21/assets/dotfiles.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -o /home/`whoami`/.bashrc https://raw.githubusercontent.com/gelrii/dotfiles/master/.bashrc
-curl -o /home/`whoami`/.bash_aliases https://raw.githubusercontent.com/gelrii/dotfiles/master/.bash_aliases
